82d4caa701c3732852e0c512bb8cc31667830787
Paweł Salawa <pawelsalawa@gmail.com>
2025-01-04 22:39:17 +0100

    Attempt to fix Release creation

--- scripts/create_draft_release.sh
+++ scripts/create_draft_release.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-PAYLOAD=$(cat $1)
-
-temp_file=$(mktemp "${RUNNER_TEMP}/tempfile.XXXXXX")
+PAYLOAD="$1"
 
 echo "Release URL: https://api.github.com/repos/$REPO/releases" >&2
 echo "Release payload: $PAYLOAD" >&2
       
-curl -s -L \
+resp=$(curl -s -L \
       --trace-ascii /dev/stderr \
       -X POST \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       https://api.github.com/repos/$REPO/releases \
-      -d "$PAYLOAD" > $temp_file
+      -d "$PAYLOAD")
 
-echo "Resp: $(cat $temp_file)" >&2
-resp=$(cat $temp_file)
+echo "Resp: $resp" >&2
 echo $(echo $resp | jq '.id')
