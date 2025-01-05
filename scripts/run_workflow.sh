c857f44ea79f7e6d7c09af38e76cdf2493385fcf
Paweł Salawa <pawelsalawa@gmail.com>
2025-01-05 23:41:13 +0100

    Debugging script.

--- scripts/run_workflow.sh
+++ scripts/run_workflow.sh
@@ -19,13 +19,18 @@ PAYLOAD=$(
 
 echo "Run Workflow - payload: $PAYLOAD" >&2
 
-curl -s -L \
+resp=$(curl -s -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$REPO/actions/workflows/$WORKFLOW/dispatches \
-  -d "$PAYLOAD"
+  -d "$PAYLOAD")
+if [ $? -ne 0 ]
+then
+    echo "The curl to run workflow has ended with error:\n$resp" >&2
+    exit 1
+fi
 
 sleep 1
 url=https://api.github.com/repos/$REPO/actions/workflows/$WORKFLOW/runs?created=\>$child_dt
