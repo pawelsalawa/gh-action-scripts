7e92d53325b483a9fa6e7b3430f0f59ada70f739
Paweł Salawa <pawelsalawa@gmail.com>
2025-01-06 12:06:55 +0100

    Upload script returns URL. Added script for release updating

---
+++ scripts/update_release_body.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RELEASE_ID=$1
+BODY=$2
+
+payload=$(
+    js -n -c \
+        --arg body "$BODY" \
+        '{"body": $body}'
+    )
+curl -s -L \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$REPO/releases/$RELEASE_ID \
+  -d $payload
--- scripts/upload_artifact.sh
+++ scripts/upload_artifact.sh
@@ -5,11 +5,13 @@ FILE=$2
 
 fname=$(basename "$FILE")
 
-curl -s -L \
+resp=$(curl -s -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -H "Content-Type: application/octet-stream" \
   "https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$fname" \
-  --data-binary "@$FILE"
+  --data-binary "@$FILE")
+
+echo $resp | jq -r '.browser_download_url'
